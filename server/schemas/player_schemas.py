bbf893d47f3b05030ffcb0fbefd80420ea5f92b1
archie955 <archiesmedley@gmail.com>
2026-06-29 17:23:59 +0100

    create alternative endpoint depending on logic partition (back vs frontend)

--- server/schemas/player_schemas.py
+++ server/schemas/player_schemas.py
@@ -20,3 +20,8 @@ class Player(BaseModel):
 class Team_Players(BaseModel):
     players: list[Player]
     model_config = config
+
+
+class Alt_Team_Players(Team_Players):
+    team_id: int
+    model_config = config
--- server/services/player_service.py
+++ server/services/player_service.py
@@ -1,4 +1,5 @@
 import logging
+from random import randint
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -9,6 +10,8 @@ from models import models
 from schemas import player_schemas
 
 logger = logging.getLogger(__name__)
+
+NUMBER_OF_TEAMS = 100
 
 
 async def get_team_players(
@@ -36,3 +39,29 @@ async def get_team_players(
     players_list = [player_schemas.Player.model_validate(p) for p in team.players]
 
     return player_schemas.Team_Players(players=players_list)
+
+
+async def get_player_choices(db: AsyncSession) -> player_schemas.Alt_Team_Players:
+    rand = randint(0, NUMBER_OF_TEAMS - 1)
+    team = (
+        await db.execute(
+            select(models.Team)
+            .options(selectinload(models.Team.players))
+            .where(models.Team.id == rand)
+        )
+    ).scalar_one_or_none()
+
+    if not team:
+        raise DataNotFoundError(datatype="Team")
+
+    if not team.players:
+        raise DataNotFoundError(datatype="Players")
+
+    logger.info(
+        "Players fetched",
+        extra={"team_id": rand, "players_fetched": len(team.players)},
+    )
+
+    players_list = [player_schemas.Player.model_validate(p) for p in team.players]
+
+    return player_schemas.Alt_Team_Players(team_id=rand, players=players_list)
